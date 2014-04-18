855204ea16e5a4acfe5ac614697587b43cdb430e
ShiZhan <g.shizhan.g@gmail.com>
2014-04-18 13:08:47 -0500

    put loader script back

---
+++ jconsole.cmd
@@ -0,0 +1,21 @@
+@REM launcher script
+@REM Shi.Zhan @ 2013
+@REM use 'sbt copy-dep' to collect dependencies in "target\scala-2.10\lib"
+@REM load compiled classes from "target\scala-2.10" directly for development
+@REM if deploy with individual "jar" libs, update "set LIB" to lib directory.
+@echo off
+setlocal
+
+set JAVA_OPTS=%JAVA_OPTS% -Xmx2g
+set CP=
+set FSC_ROOT=%~dp0
+@REM set FSC_DATA=r:/data
+set LIB=%FSC_ROOT%target\scala-2.10\lib\
+for /f %%i in ('dir /b %LIB%') do call :concat %%i
+scala -cp "%CP%;%FSC_ROOT%target/scala-2.10/classes" JenaConsole %1 %2 %3 %4 %5 %6 %7 %8 %9
+endlocal
+goto :eof
+
+:concat
+set CP=%CP%%LIB%%1;
+goto :eof
--- project/Build.scala
+++ project/Build.scala
@@ -8,10 +8,22 @@ object MyBuild extends Build {
     scalaVersion := "2.10.3"
   )
 
+  lazy val copyDependencies = TaskKey[Unit]("copyDep")
+
+  def copyDepTask = copyDependencies <<= (update, crossTarget, scalaVersion) map {
+    (updateReport, out, scalaVer) =>
+    updateReport.allFiles foreach { srcPath =>
+      val destPath = out / "lib" / srcPath.getName
+      IO.copyFile(srcPath, destPath, preserveLastModified=true)
+    }
+  }
+
   lazy val JenaConsole = Project(
     id = "JenaConsole",
     base = file("."),
     settings = Defaults.defaultSettings ++
-    sbtassembly.Plugin.assemblySettings
+    sbtassembly.Plugin.assemblySettings ++ Seq(
+      copyDepTask
+    )
   )
 }
