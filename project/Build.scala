b057e601e3c364da6e5bc5aaa91fe6f58c808f66
ShiZhan <g.shizhan.g@gmail.com>
2015-08-13 16:39:35 +0800

    project definition

--- project/Build.scala
+++ project/Build.scala
@@ -1,11 +1,31 @@
 import sbt._
 import Keys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
 
 object MyBuild extends Build {
-  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
-    version := "0.1-SNAPSHOT",
+  val scalaV = Option(System.getProperty("scala.version")).getOrElse("2.11.6")
+  val jenaV  = "2.12.1"
+  val tdbV   = "1.1.1"
+  val slf4jV = "1.7.12"
+  val log4jV = "1.2.17"
+
+  lazy val commonSettings = Defaults.defaultSettings ++ Seq(
+    version      := "0.1-SNAPSHOT",
     organization := "com.simba",
-    scalaVersion := Option(System.getProperty("scala.version")).getOrElse("2.10.6")
+    scalaVersion := scalaV,
+    scalacOptions in Compile ++= Seq(
+      "-encoding", "UTF-8",
+      "-deprecation", "-feature", "-unchecked",
+      "-Xlint"),
+    libraryDependencies ++= Seq(
+      "org.apache.jena" % "jena-core" % jenaV excludeAll(ExclusionRule(organization = "org.slf4j")), 
+      "org.apache.jena" % "jena-arq"  % jenaV excludeAll(ExclusionRule(organization = "org.slf4j")), 
+      "org.apache.jena" % "jena-tdb"  % tdbV excludeAll(ExclusionRule(organization = "org.slf4j")),
+      "org.slf4j" % "slf4j-api"     % slf4jV,
+      "org.slf4j" % "slf4j-log4j12" % slf4jV,
+      "log4j"     % "log4j"         % log4jV
+    )
   )
 
   lazy val copyDependencies = TaskKey[Unit]("copyDep")
@@ -18,12 +38,16 @@ object MyBuild extends Build {
     }
   }
 
+  lazy val buildSettings = commonSettings
+
   lazy val JenaConsole = Project(
     id = "JenaConsole",
     base = file("."),
-    settings = Defaults.defaultSettings ++
-    sbtassembly.Plugin.assemblySettings ++ Seq(
-      copyDepTask
+    settings = commonSettings ++
+    sbtassembly.Plugin.assemblySettings ++
+    Seq(
+      copyDepTask,
+      logLevel in assembly := Level.Warn
     )
   )
 }
